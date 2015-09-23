f9920863b9dfcf5753ab0869da740901b2a703ff
sajdakk <kristof.sajdak@gmail.com>
2015-09-23 23:47:55 +0200

    added tests which sets up a post route in a Hapi server and fires of requests to validate the schema functionality

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -4,26 +4,26 @@ const hh = require('../lib/routes'),
     Lab = require('lab'),
     Hapi = require('hapi'),
     _ = require('lodash'),
-    expect = Code.expect
+    expect = Code.expect,
+    uuid = require('node-uuid')
 
 var lab = exports.lab = Lab.script()
-
 
 const schema = {
     type: 'brands',
     attributes: {
-        code: A.string().min(4).max(10),
+        code: A.string().min(2).max(10),
         description: A.string()
     }
 }
 
 lab.experiment('hh.get is called', ()=> {
 
-    const route = hh.get(schema);
+    const route = hh.get(schema)
 
     lab.experiment('the route config/validate section', ()=> {
 
-        const query = route.config.validate.query;
+        const query = route.config.validate.query
 
         lab.test('has a query block defined', (done)=> {
             expect(query).to.not.be.undefined()
@@ -46,51 +46,52 @@ lab.experiment('hh.get is called', ()=> {
     lab.experiment('result can be registered with a hapi server', ()=> {
 
         route.handler = function (req, reply) {
-            reply();
+            reply()
         }
 
-        var server = new Hapi.Server();
-        server.connection();
-        server.route(route);
+        var server = new Hapi.Server()
+        server.connection()
+        server.route(route)
 
         lab.test('query with a valid id works', (done)=> {
-            server.inject('/brands?id=b6c37144-c29f-49a9-8463-d5a1da8583fc', function (res) {
-                expect(res.statusCode).to.equal(200);
-                done();
-            });
+            server.inject(`/brands?id=${uuid.v4()}`, function (res) {
+                expect(res.statusCode).to.equal(200)
+                done()
+            })
         })
 
         lab.test('query with an invalid id returns a 400', (done)=> {
             server.inject('/brands?id=foobar', function (res) {
-                expect(res.statusCode).to.equal(400);
-                done();
-            });
+                expect(res.statusCode).to.equal(400)
+                done()
+            })
         })
 
         lab.test('query with a valid code returns a 200', (done)=> {
             server.inject('/brands?code=1234', function (res) {
-                expect(res.statusCode).to.equal(200);
-                done();
-            });
+                expect(res.statusCode).to.equal(200)
+                done()
+            })
         })
 
         lab.test('query with unknowns are allowed', (done)=> {
             server.inject('/brands?foo=bar', function (res) {
-                expect(res.statusCode).to.equal(200);
-                done();
-            });
+                expect(res.statusCode).to.equal(200)
+                done()
+            })
         })
 
-    });
+    })
 
 
-});
+})
 
 lab.experiment('hh.post is called,', ()=> {
 
+    const route = hh.post(schema)
+
     lab.experiment('the route config/validate section', ()=> {
 
-        const route = hh.post(schema)
         const payload = route.config.validate.payload
 
         lab.test('has a payload object defined', (done)=> {
@@ -110,10 +111,57 @@ lab.experiment('hh.post is called,', ()=> {
         })
     })
 
-})
+    lab.experiment('result can be registered with a hapi server', ()=> {
 
-function expectKid(joiObject, key) {
-    expect(_.filter(joiObject._inner.children, kid => {
-        return kid.key == key
-    })).to.have.length(1);
-}
+        route.handler = function (req, reply) {
+            reply('').code(201)
+        }
+
+        var server = new Hapi.Server()
+        server.connection()
+        server.route(route)
+
+        const validBrand = {
+            data: {
+                type: 'brands',
+                attributes: {
+                    code: 'MF',
+                    description: 'Massey Ferguson'
+                }
+            }
+        }
+
+        lab.test('post with a valid payload returns success', (done)=> {
+            server.inject({url:'/brands', method:'POST', payload:validBrand}, function (res) {
+                expect(res.statusCode).to.equal(201)
+                done()
+            })
+        })
+
+        lab.test('post with a valid payload and a client generated id returns success', (done)=> {
+            const invalidBrand = _.chain(validBrand).cloneDeep().set('data.attributes.id', uuid.v4())
+            server.inject({url:'/brands', method:'POST', payload:invalidBrand}, function (res) {
+                expect(res.statusCode).to.equal(201)
+                done()
+            })
+        })
+
+        lab.test('post with non declared attribute fails with a 400', (done)=> {
+            const invalidBrand = _.chain(validBrand).cloneDeep().set('data.attributes.bogus', 'foobar')
+            server.inject({url:'/brands', method:'POST', payload:invalidBrand}, function (res) {
+                expect(res.statusCode).to.equal(400)
+                done()
+            })
+        })
+
+        lab.test('post with an invalid attribute value fails with a 400', (done)=> {
+            const invalidBrand = _.chain(validBrand).cloneDeep().set('data.attributes.code', 'M')
+            server.inject({url:'/brands', method:'POST', payload:invalidBrand}, function (res) {
+                expect(res.statusCode).to.equal(400)
+                done()
+            })
+        })
+
+    })
+
+})
